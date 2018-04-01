8f55ce6f2ce9535415d57a6ad9e9bc0e7afe4d30
AlexPetrova <a.petrova95@yahoo.com>
2018-04-01 23:55:38 +0300

    AddCard view

--- BankSystem/BankSystem.Web/Controllers/CardController.cs
+++ BankSystem/BankSystem.Web/Controllers/CardController.cs
@@ -1,5 +1,7 @@
-﻿using BankSystem.DTO;
+﻿using AutoMapper;
+using BankSystem.DTO;
 using BankSystem.DTO.ClientModels;
+using BankSystem.Services;
 using BankSystem.Services.Contracts;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,8 @@ namespace BankSystem.Web.Controllers
             this.cardService = cardService ?? throw new ArgumentNullException();
         }
 
+        public static IEnumerable<BankAccountReadModel> BankAccounts { get; set; }
+
         [Authorize]
         public ActionResult AllCards()
         {
@@ -23,6 +27,39 @@ namespace BankSystem.Web.Controllers
             ClientModel clientModel = new ClientModel() { UserName = userName };
             IEnumerable<CardModel> allCardsOfUser = this.cardService.GetAllCardsOfUser(clientModel);
             return this.View(allCardsOfUser);
+        }
+
+        [Authorize]
+        public ActionResult AddCard()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCard(string number, string pin, DateTime expirationDate, string secretNumber, int idOfBankAccount)
+        {
+            string cardNumber = number;
+            //DateTime expirationDate = expirationDate;
+            int bankAccID = idOfBankAccount;
+
+            CardModel cardToAdd = new CardModel()
+            {
+                Account = new BankAccountModel() { Id = idOfBankAccount },
+                ExpirationDate = expirationDate,
+                Number = number,
+                Pin = pin,
+                SecretNumber = secretNumber
+            };
+
+            cardService.AddCard(cardToAdd);
+            this.PartialView("GetDropDownListOfBankAccountIDs");
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult RemoveCard()
+        {
+            return this.View();
         }
     }
 }
